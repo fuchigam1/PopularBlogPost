7544dd0dc8e87e177887985a282563d35153aa6d
arata <contact@materializing.net>
2015-09-05 19:30:35 +0900

    add setting ファイル

---
+++ Config/setting.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * [ADMIN] PopularBlogPost
+ *
+ * @link			http://www.materializing.net/
+ * @author			arata
+ * @package			PopularBlogPost
+ * @license			MIT
+ */
+/**
+ * システムナビ
+ */
+$config['BcApp.adminNavi.popular_blog_post'] = array(
+		'name'		=> 'ポピュラーブログポスト プラグイン',
+		'contents'	=> array(
+			array('name' => 'アクセス一覧',
+				'url' => array(
+					'admin' => true,
+					'plugin' => 'popular_blog_post',
+					'controller' => 'popular_blog_posts',
+					'action' => 'index')
+			),
+			array('name' => '設定一覧',
+				'url' => array(
+					'admin' => true,
+					'plugin' => 'popular_blog_post',
+					'controller' => 'popular_blog_post_configs',
+					'action' => 'index')
+			)
+	)
+);
--- View/Elements/admin/popular_blog_posts/index_row.php
+++ View/Elements/admin/popular_blog_posts/index_row.php
@@ -15,10 +15,11 @@
 	<?php $this->BcBaser->link($this->BcBaser->getImg('admin/icn_tool_publish.png', array('width' => 24, 'height' => 24, 'alt' => '有効', 'class' => 'btn')),
 			array('action' => 'ajax_publish', $data['PopularBlogPost']['id']), array('title' => '有効', 'class' => 'btn-publish')) ?>
 
-	<?php // ブログ記事編集画面へ移動 ?>
-	<?php $this->BcBaser->link($this->BcBaser->getImg('admin/icn_tool_check.png', array('width' => 24, 'height' => 24, 'alt' => 'ブログ記事編集', 'class' => 'btn')),
-			array('admin' => true, 'plugin' => 'blog', 'controller' => 'blog_posts', 'action' => 'edit', $data['BlogPost']['blog_content_id'], $data['BlogPost']['id']),
-			array('title' => 'ブログ記事編集'), 'ブログ記事編集画面へ移動します。よろしいですか？', false); ?>
+	<?php // ブログ記事画面へ移動 ?>
+	<?php echo $this->Blog->getPostLink($data,
+		$this->BcBaser->getImg('admin/icn_tool_check.png', array('width' => 24, 'height' => 24, 'alt' => 'ブログ記事編集', 'class' => 'btn')),
+		array('target' => '_blank')
+	) ?>
 
 	<?php $this->BcBaser->link($this->BcBaser->getImg('admin/icn_tool_edit.png', array('width' => 24, 'height' => 24, 'alt' => '編集', 'class' => 'btn')),
 			array('action' => 'edit', $data['PopularBlogPost']['id']), array('title' => '編集')) ?>
@@ -26,7 +27,7 @@
 		array('action' => 'ajax_delete', $data['PopularBlogPost']['id']), array('title' => '削除', 'class' => 'btn-delete')) ?>
 	</td>
 	<td style="width: 45px;">
-		<?php echo $this->BcBaser->link($data['PopularBlogPost']['id'], array('action' => 'edit', $data['PopularBlogPost']['id']), array('title' => '編集')) ?>
+		<?php echo $data['PopularBlogPost']['id'] ?>
 	</td>
 	<td>
 		<?php echo $blogContentDatas[$data['PopularBlogPost']['blog_content_id']] ?>
