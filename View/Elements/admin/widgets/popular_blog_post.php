5aaeb3bb4f3042631b792e9722d04be8e5faa845
arata <contact@materializing.net>
2016-05-14 17:41:34 +0900

    clean 整形

--- View/Elements/admin/widgets/popular_blog_post.php
+++ View/Elements/admin/widgets/popular_blog_post.php
@@ -7,7 +7,7 @@
  * @package			PopularBlogPost
  * @license			MIT
  */
-$title = '人気記事ランキング';
+$title		 = '人気記事ランキング';
 $description = '閲覧数の多い記事を表示します。';
 ?>
 <?php echo $this->BcForm->input($key . '.display_view_count', array('type' => 'checkbox', 'label' => 'カウント数を表示する')) ?>
@@ -16,14 +16,20 @@ $description = '閲覧数の多い記事を表示します。';
 <?php echo $this->BcForm->input($key . '.count', array('type' => 'text', 'size' => 6, 'default' => 5)) ?>&nbsp;件
 <br />
 <?php echo $this->BcForm->label($key . '.order', '並び順') ?>&nbsp;
-<?php echo $this->BcForm->input($key . '.order', array(
-	'type' => 'select',
-	'options' => array('DESC' => 'アクセス数の多い順', 'ASC' => 'アクセス数の少ない順'),
-)) ?>
+<?php
+echo $this->BcForm->input($key . '.order', array(
+	'type'		 => 'select',
+	'options'	 => array('DESC' => 'アクセス数の多い順', 'ASC' => 'アクセス数の少ない順'),
+))
+?>
 <br />
 <?php echo $this->BcForm->label($key . '.blog_content_id', 'ブログ') ?>&nbsp;
-<?php echo $this->BcForm->input($key . '.blog_content_id', array(
-	'type' => 'select',
-	'options' => $this->BcForm->getControlSource('Blog.BlogContent.id'),
-	'empty' => '指定しない',
-)) ?>
+<?php
+echo $this->BcForm->input($key . '.blog_content_id', array(
+	'type'		 => 'select',
+	'options'	 => $this->BcForm->getControlSource('Blog.BlogContent.id'),
+	'empty'		 => '指定しない',
+))
+?>
+<br />
+<?php echo $this->BcForm->input($key . '.restrict_archive_category', array('type' => 'checkbox', 'label' => '表示中のカテゴリーアーカイブ内の記事でランキング表示を行う')) ?>
